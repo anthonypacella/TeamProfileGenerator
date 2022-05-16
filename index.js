e1dbd08920a0c347fda073f7f4ca93128d90f108
anthonypacella <anthonyapacella@gmail.com>
2022-05-15 22:48:43 -0400

    updated index.js

---
+++ index.js
@@ -0,0 +1,235 @@
+//dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { exit } = require('process');
+
+//class imports
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+let internList = [];
+let engineerList = [];
+var managerObj;
+let managerHTML = '';
+let engineerHTML = '';
+let internHTML = '';
+
+function init() {
+    managerQuestions();
+}
+
+function managerQuestions() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the team managers\'s name?',
+            name: 'managerName'
+        },
+
+        {
+            type: 'input',
+            message: `What is the managers\'s ID number?`,
+            name: 'managerId'
+        },
+        {
+            type: 'input',
+            message: `What is the managers\'s email address?`,
+            name: 'managerEmail'
+        },
+        {
+            type: 'input',
+            message: `What is the managers\'s office number?`,
+            name: 'managerOfficeNumber'
+        },
+    ])
+    .then(function managerAnswerHandler(data) {
+        let managerObj = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNumber);
+        console.log(managerObj);
+        moreTeamMembers();
+    })
+};
+
+function moreTeamMembers() {
+    managerObj = managerObj;
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add more employees?',
+            choices: ['Yes', 'No'],
+            name: 'addEmployees'
+        }
+    ])
+    .then(function teamMemberQuestionHandler(data) {
+        if (data.addEmployees === 'Yes') {
+            findOutRole();
+        }
+        else {
+            drawTeamProfile();
+            // exit();
+        }
+    })
+};
+
+function findOutRole() {
+    inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    message: 'What is the role of the employee?',
+                    choices: ['Engineer', 'Intern'],
+                    name: 'employeeRole'
+                },
+            ])
+            .then(function findOutRoleHandler(data) {
+                let employeeRole = data.employeeRole;
+                teamMemberQuestions(employeeRole);
+            })
+};
+
+function teamMemberQuestions(employeeRole) {
+    if (employeeRole === "Engineer") {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the engineer\'s name?',
+                name: 'engineerName'
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s employee ID?',
+                name: 'engineerId'
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s email address?',
+                name: 'engineerEmail'
+            },
+            {
+                type: 'input',
+                message: 'What is the engineer\'s github username?',
+                name: 'engineerGithub'
+            }
+        ])
+        .then(function teamMemberQuestionsHandler(data) {
+            let newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+            engineerList.push(newEngineer);
+        })
+    }
+    else if (employeeRole === "Intern") {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the intern\'s name?',
+                name: 'internName'
+            },
+            {
+                type: 'input',
+                message: 'What is the intern\'s employee ID?',
+                name: 'internId'
+            },
+            {
+                type: 'input',
+                message: 'What is the intern\'s email address?',
+                name: 'internEmail'
+            },
+            {
+                type: 'input',
+                message: 'Where is the intern attending university?',
+                name: 'internSchool'
+            }
+        ])
+        .then(function teamMemberQuestionsHandler(data) {
+            let newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internGithub);
+            internList.push(newIntern);
+        })
+    }
+};
+
+init();
+
+function drawTeamProfile() {
+    let topHTML =  `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossorigin="anonymous"
+        />
+    </head>
+    <body style = 'background-color: lightblue;'>
+        <header>
+            <h1 class = "mb-3 text-center">Team Members:</h1>
+        </header>
+    
+        <div class = "container-fluid">
+            <div class = "row" style = 'padding: 10px; margin:3px;'>`;
+
+        managerHTML = `<div class = "card" style = 'width: 18rem;'>
+            <div class = 'card-body'>
+                <h5 class = 'card-title'>${Manager.name}</h5>
+                <h6 class = 'card-subtitle mb-2 text-muted'>Manager</h6>
+                <ul class = 'list-group list-group-flush'>
+                    <li class = 'list-group-item'>Employee ID: ${Manager.id}</li>
+                    <li class = 'list-group-item'>Email Address: <a href="mailto:${Manager.email}">emailaddress</a></li>
+                    <li class = 'list-group-item'>Office Number: ${Manager.officeNumber}</li>
+                </ul> 
+            </div>
+        </div>`
+        
+    
+
+    engineerList.forEach((engineer) => {
+
+            engineerHTML = engineerHTML + `<div class = "card" style = 'width: 18rem;'>
+            <div class = 'card-body'>
+                <h5 class = 'card-title'>${engineer.employeeName}</h5>
+                <h6 class = 'card-subtitle mb-2 text-muted'>Manager</h6>
+                <ul class = 'list-group list-group-flush'>
+                    <li class = 'list-group-item'>Employee ID: ${engineer.employeeName.employeeId}</li>
+                    <li class = 'list-group-item'>Email Address: <a href="mailto:${engineer.employeeName.employeeEmail}">emailaddress</a></li>
+                    <li class = 'list-group-item'>Github Username: ${engineer.employeeName.engineerGithub}</li>
+                </ul> 
+            </div>
+        </div>`
+    });
+
+    internList.forEach((intern) => {
+
+        internHTML = internHTML + `<div class = "card" style = 'width: 18rem;'>
+        <div class = 'card-body'>
+            <h5 class = 'card-title'>${intern.employeeName}</h5>
+            <h6 class = 'card-subtitle mb-2 text-muted'>Manager</h6>
+            <ul class = 'list-group list-group-flush'>
+                <li class = 'list-group-item'>Employee ID: ${intern.employeeName.employeeId}</li>
+                <li class = 'list-group-item'>Email Address: <a href="mailto:${intern.employeeName.employeeEmail}">emailaddress</a></li>
+                <li class = 'list-group-item'>University: ${intern.employeeName.internSchool}</li>
+            </ul> 
+        </div>
+    </div>`
+});
+
+let bottomHTML = `</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</body>
+</html>`;
+
+let fullHTML = topHTML + managerHTML + engineerHTML + internHTML + bottomHTML;
+
+console.log(managerObj);
+fs.writeFile('./index.html', fullHTML, (err) => err ? console.error(err) : console.log('index.html created'));
+
+}
